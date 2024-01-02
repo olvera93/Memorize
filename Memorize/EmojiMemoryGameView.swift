fa55bab86d178bb18ee7eda8b593c8b0318e6a29
olvera93 <olvera9302@outlook.com>
2024-01-02 13:18:21 -0600

    [ADD] The logic to maching the cards

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -14,6 +14,7 @@ struct EmojiMemoryGameView: View {
         VStack {
             ScrollView {
                 cards
+                    .animation(.default, value: viewModel.cards)
             }
             
             Button("Shuffle") {
@@ -26,10 +27,14 @@ struct EmojiMemoryGameView: View {
     
     var cards: some View {
         LazyVGrid(columns: [GridItem(.adaptive(minimum: 85), spacing: 0)], spacing: 0) {
-            ForEach(viewModel.cards.indices, id: \.self) { index in
-                CardView(viewModel.cards[index])
+            ForEach(viewModel.cards) { card in
+                
+                CardView(card)
                     .aspectRatio(2/3, contentMode: .fit)
                     .padding(4)
+                    .onTapGesture {
+                        viewModel.choose(card)
+                    }
             }
         }.foregroundColor(.orange)
     }
@@ -58,6 +63,8 @@ struct CardView: View {
             .opacity(card.isFaceUp ? 1 : 0)
             base.fill().opacity(card.isFaceUp ? 0: 1)
         }
+        
+        .opacity(card.isFaceUp || !card.isMatched ? 1 : 0 )
     }
 }
 
